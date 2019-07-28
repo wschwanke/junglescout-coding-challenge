53ad93a32d5769234885461b1bb1052e2bd92302
Will Schwanke <wschwanke@gmail.com>
2019-07-28 16:46:39 -0500

    Add customizer settings for the homepage hero image.

--- app/wp-content/themes/junglescout-jslaunch/resources/views/partials/homepage-header.blade.php
+++ app/wp-content/themes/junglescout-jslaunch/resources/views/partials/homepage-header.blade.php
@@ -1,19 +1,17 @@
-<div class="homepage--header row pt-6 pb-6 mb-6" style="background-image: url('<?php echo get_theme_mod("js_launch_hero_image") ?>')">
+<div class="homepage--header row pt-6 pb-6 mb-6" style="background-image: url('<?= get_theme_mod("js_launch_hero_image") ?>')">
   <div class="container">
     <div class="row homepage--header__cta">
       <div class="col-12 col-md-6 mb-5">
-        <h2 class="h1 mb-4">Launch your Amazon FBA business with the right data</h2>
-        <p class="w-75 mb-0 font-weight-500">Jungle Scout is the only tool that Amazon FBA sellers trust for production research.</p>
+        <h2 class="h1 mb-4"><?= get_theme_mod("js_launch_hero_heading") ?></h2>
+        <p class="w-75 mb-0 font-weight-500"><?= get_theme_mod("js_launch_hero_subtext") ?></p>
       </div>
       <div class="homepage--header__testimonial col-12 col-md-4 offset-0 offset-md-1 mb-5 mt-md-2">
-        <p class="homepage--header__testimonial--content">"Jungle Scout is the ONE tool I can not live without. There's ways to use it that
-        simply blow you away once you realize the power of what they can do forr your
-        business. A must-have tool for any Amazon seller!"</p>
+        <p class="homepage--header__testimonial--content"><?= get_theme_mod("js_launch_hero_review") ?></p>
         <div class="homepage--header__testimonial--person">
-          <img src="./scott-volker" />
-          <div>
-            <span>Scott Volker</span>
-            <small>The Amazing Seller</small>
+          <img src="<?= get_theme_mod("js_launch_hero_reviewer_image") ?>" />
+          <div class="homepage--header__testimonial--nameloc">
+            <span><?= get_theme_mod("js_launch_hero_reviewer_name") ?></span>
+            <small><?= get_theme_mod("js_launch_hero_reviewer_title") ?></small>
           </div>
         </div>
       </div>
